3ac8d7e5724f47c32c3db1d778e4fa6ceaf462d6
Camilo Hurtado <info@camihurs.biz>
2023-09-09 16:52:25 -0500

    Creamos funciones en mokepon.js

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,8 +1,18 @@
 //alert("hola, mundo JS") //usada simplemente para verificar inicialmente el funcionamiento del archivo .js
 
+function iniciarJuego(){
+    //document es el archivo html
+    let botonMascotaJugador = document.getElementById("boton-mascota")
+    botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador)
+}
+
+
 function seleccionarMascotaJugador(){
     alert("Seleccionaste tu mascota")
 }
 
-let botonMascotaJugador = document.getElementById("boton-mascota")
-botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador)
+
+//window es el navegador
+//Hacemos esto para que este archivo js se ejecute solamente cuando el archivo html se ha terminado
+//de cargar complemente
+window.addEventListener('load',iniciarJuego)
