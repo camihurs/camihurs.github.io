f7df159780b6f168551482a858be5c687cfa216e
Camilo Hurtado <info@camihurs.biz>
2023-09-09 16:41:52 -0500

    Agregamos eventListener al bot seleccionar-mascota

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,1 +1,8 @@
-alert("hola, mundo JS")
+//alert("hola, mundo JS") //usada simplemente para verificar inicialmente el funcionamiento del archivo .js
+
+function seleccionarMascotaJugador(){
+    alert("Seleccionaste tu mascota")
+}
+
+let botonMascotaJugador = document.getElementById("boton-mascota")
+botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador)
